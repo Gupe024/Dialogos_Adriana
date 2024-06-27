168dfe179a4a027d3ac58973c7e9b8e46aa9d059
Canul <canuladriana24@gmail.com>
2024-06-26 21:12:15 -0600

    segundo commit

--- app/src/main/java/com/example/dialogosapp/MainActivity.java
+++ app/src/main/java/com/example/dialogosapp/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.dialogosapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button DialogoBasic;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        DialogoBasic = findViewById(R.id.Dialog_basico);
+        DialogoBasic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogo();
+            }
         });
+    }
+    private void mostrarDialogo() {
+        AlertDialog.Builder constructorDialogo = new AlertDialog.Builder(MainActivity.this);
+        constructorDialogo.setTitle("Diálogo Básico");
+        constructorDialogo.setMessage("¿Quieres aceptar o cancelar?");
+        constructorDialogo.setPositiveButton("Aceptar", (dialog, which) -> {
+            Toast.makeText(MainActivity.this, "Has aceptado", Toast.LENGTH_SHORT).show();
+        });
+        constructorDialogo.setNegativeButton("Cancelar", (dialog, which) -> {
+            Toast.makeText(MainActivity.this, "Has cancelado", Toast.LENGTH_SHORT).show();
+        });
+        constructorDialogo.show();
     }
 }
