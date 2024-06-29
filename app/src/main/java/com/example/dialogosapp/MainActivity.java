d925b6366ec6cb46eec4b6bf6a7db09be134591d
Canul <canuladriana24@gmail.com>
2024-06-28 18:31:56 -0600

    Cuarto commit

--- app/src/main/java/com/example/dialogosapp/MainActivity.java
+++ app/src/main/java/com/example/dialogosapp/MainActivity.java
@@ -4,33 +4,39 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Switch;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button DialogoBasic;
 
+    Button DialogoBasico, DialogoPersonalizado;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DialogoBasic = findViewById(R.id.Dialog_basico);
-        DialogoBasic.setOnClickListener(new View.OnClickListener() {
+        DialogoBasico = findViewById(R.id.Dialog_basico);
+        DialogoPersonalizado = findViewById(R.id.Dialogo_personalizado);
+
+        DialogoBasico.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mostrarDialogo();
+                mostrarDialogoBasico();
+            }
+        });
+
+        DialogoPersonalizado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoPersonalizado();
             }
         });
     }
-    private void mostrarDialogo() {
-        AlertDialog.Builder constructorDialogo = new AlertDialog.Builder(MainActivity.this);
+
+    private void mostrarDialogoBasico() {
+        AlertDialog.Builder constructorDialogo = new AlertDialog.Builder(MainActivity.this, R.style.CustomDialogTheme);
         constructorDialogo.setTitle(R.string.Dialogo_titulo);
         constructorDialogo.setMessage(R.string.Muestra_mensaje);
         constructorDialogo.setPositiveButton(R.string.Aceptar, new DialogInterface.OnClickListener() {
@@ -48,6 +54,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
         constructorDialogo.show();
+    }
 
+    private void mostrarDialogoPersonalizado() {
+        AlertDialog.Builder constructorDialogo = new AlertDialog.Builder(MainActivity.this, R.style.CustomDialogTheme);
+        constructorDialogo.setTitle(R.string.Dialogo_titulo_personalizado);
+
+        View dialogView = getLayoutInflater().inflate(R.layout.dialogo_personalizado, null);
+        constructorDialogo.setView(dialogView);
+
+        constructorDialogo.setPositiveButton(R.string.Aceptar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, R.string.T_aceptar, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        constructorDialogo.setNegativeButton(R.string.Cancelar, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this, R.string.T_cancelar, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        constructorDialogo.show();
     }
 }
